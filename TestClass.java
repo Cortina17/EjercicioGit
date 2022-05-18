42aa4066efb9de0ba985f08a465ba622424b4ac1
diego <diego.cortinagarcia@gmail.com>
2022-05-18 15:55:07 +0200

    first commit. Created two java files with a function each

---
+++ TestClass.java
@@ -0,0 +1,11 @@
+public class TestClass {
+	private final String message;
+
+	public TestClass() {
+		this.message = "Hola mundo";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
---
+++ pruebados.java
@@ -0,0 +1,11 @@
+public class pruebados {
+	private final String message;
+
+	public pruebados() {
+		this.message = "Hola mundo";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
